29563e27cab1b0539ea2b68ef9db0461de2def43
Geovanna-Albuquerque <geovanna.albuquerque1@aluno.ce.gov.br>
2024-09-18 13:57:52 -0300

    verificar se o numero é positivo ou negativo

---
+++ atividade 13 .py
@@ -0,0 +1,6 @@
+
+#solicita ao usuario digitar um numero inteiro 
+numero = int(input('digite um numero inteiro:'))
+
+#verificar se o numero é positivo ou negativo 
+print(numero, 'é um numero', 'positivo.' if numero >= 0 else 'negativo.')
